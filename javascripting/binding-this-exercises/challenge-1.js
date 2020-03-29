953fa6fe8e30988988799c30b184b331aa309a5d
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-03-29 13:18:13 +0200

    simpler-test-cases

--- javascripting/binding-this-exercises/challenge-1.js
+++ javascripting/binding-this-exercises/challenge-1.js
@@ -3,35 +3,35 @@
 function writeMe(arg) { };
 
 // don't change any code below this comment
-const obj1 = { thing: undefined };
+const obj1 = { thing: 1 };
 const bound1 = writeMe.bind(obj1);
 
 const result1a = bound1(4);
-const test1a = Number.isNaN(result1a);
+const test1a = result1a === 5;
 console.assert(test1a, 'Test 1.A');
 
 const result1b = bound1('');
-const test1b = result1b === 'undefined';
+const test1b = result1b === '1';
 console.assert(test1b, 'Test 1.B');
 
 
-const obj2 = { thing: true };
+const obj2 = { thing: 2 };
 const bound2 = writeMe.bind(obj2);
 
 const result2a = bound2(true);
-const test2a = result2a === 2;
+const test2a = result2a === 3;
 console.assert(test2a, 'Test 2.A');
 
 const result2b = bound2(-1);
-const test2b = result2b === 0;
+const test2b = result2b === 1;
 console.assert(test2b, 'Test 2.B');
 
 
-const obj3 = { thing: null };
+const obj3 = { thing: 3 };
 const bound3 = writeMe.bind(obj3);
 
-const result3a = bound3(0);
-const test3a = result3a === 0;
+const result3a = bound3(null);
+const test3a = result3a === 3;
 console.assert(test3a, 'Test 3.A');
 
 const result3b = bound3(undefined);
