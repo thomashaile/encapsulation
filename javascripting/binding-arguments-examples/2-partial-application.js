9663baddd2a5980df6b6f1dd6b0b54b2f5205cd7
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-03-29 12:36:45 +0200

    bound-args

--- javascripting/binding-arguments-examples/2-partial-application.js
+++ javascripting/binding-arguments-examples/2-partial-application.js
@@ -7,35 +7,35 @@ function addToWord(boundArgument, freeArgument) {
 }
 
 const obj1 = {
-  word: ''
+  word: '1'
 };
 
 const boundObj1x = addToWord.bind(obj1, 'x');
 const boundObj1y = addToWord.bind(obj1, 'y');
 
 boundObj1x('A');
-console.assert(obj1.word === 'xA', 'Test 1');
+console.assert(obj1.word === '1xA', 'Test 1');
 
 boundObj1x('B');
-console.assert(obj1.word === 'xAxB', 'Test 2');
+console.assert(obj1.word === '1xAxB', 'Test 2');
 
 boundObj1y('C');
-console.assert(obj1.word === 'xAxByC', 'Test 3');
+console.assert(obj1.word === '1xAxByC', 'Test 3');
 
 
 const obj2 = {
-  word: ''
+  word: '2'
 };
 
 const boundObj2a = addToWord.bind(obj2, 'a');
 const boundObj2b = addToWord.bind(obj2, 'b');
 
 boundObj2a('X');
-console.assert(obj2.word === 'aX', 'Test 4');
+console.assert(obj2.word === '2aX', 'Test 4');
 
 boundObj2b('Y');
-console.assert(obj2.word === 'aXbY', 'Test 5');
+console.assert(obj2.word === '2aXbY', 'Test 5');
 
 boundObj2a('Z');
-console.assert(obj2.word === 'aXbYaZ', 'Test 6');
+console.assert(obj2.word === '2aXbYaZ', 'Test 6');
 
